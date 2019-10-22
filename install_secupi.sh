565f3e722845d4108c4cbfc266eaa38c5ddac0f6
Gal Frishman <gal@secupi.com>
2019-10-22 15:06:18 +0300

    Minor fix

--- install_secupi.sh
+++ install_secupi.sh
@@ -5,6 +5,6 @@ mkdir -p /opt/secupi/agents
 mkdir -p /opt/secupi/cache
 mkdir -p /opt/secupi/dump
 cd /opt/secupi
-chown 777 logs agents cache dump
+chmod 777 logs agents cache dump
 wget --no-check-certificate --trust-server-name $1
 chown -R yarn:hadoop /opt/secupi
